aa7173e775de16d41641b483e9986bed9d626ff9
Ishika Rana <139616363+eeeeshika21@users.noreply.github.com>
2024-07-30 16:47:30 +0530

    Add files via upload

---
+++ Week_1_Exercises/Design Patterns/AdapterPattern/src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        
+        PayPalGateway payPalGateway = new PayPalGateway();
+        PaymentProcessor payPalProcessor = new PayPalAdapter(payPalGateway);
+        payPalProcessor.processPayment(100.00);
+
+        StripeGateway stripeGateway = new StripeGateway();
+        PaymentProcessor stripeProcessor = new StripeAdapter(stripeGateway);
+        stripeProcessor.processPayment(200.00);
+    }
+}
+
